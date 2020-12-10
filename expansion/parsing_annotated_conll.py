98fbd66b9f7e3b635d27da3274f8dab91cb4bd79
Zhaoyi(Marcus) Huang <1002423907@qq.com>
2020-12-10 01:23:18 -0800

    Add files via upload

---
+++ expansion/parsing_annotated_conll.py
@@ -0,0 +1,35 @@
+'''
+The file only contains word and tagging. If we only care about LOC, the
+tagging would only have LOC and for all the other things they are O
+'''
+with open('test.txt','r') as f:
+    texts=[]
+    tags=[]
+    line=f.readline()
+    temp_words=[]
+    temp_tags=[]
+    while line:
+        if "-DOCSTART-" not in line and line != "\n":
+            tt=line.split(" ")
+            temp_words.append(tt[0])
+            tt[-1]=tt[-1].rstrip()
+            temp_tags.append(tt[-1])
+        elif line=="\n" and temp_words!=[]:
+            texts.append(" ".join(temp_words))
+            tags.append(" ".join(temp_tags))
+            temp_words=[]
+            temp_tags=[]
+        line=f.readline()
+    f.close()
+
+with open('annotated_test_sentences.txt','w') as f:
+    for i in range(len(texts)):
+        f.write(texts[i]+'\n')
+f.close()
+
+with open('annotated_test_tags.txt','w') as f:
+    for i in range(len(tags)):
+        f.write(tags[i]+'\n')
+f.close()
+
+
